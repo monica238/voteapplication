61bb824600d0f68f527e7fe8afea5da312db9483
v2learnmysore@gmail.com <v2learnmysore@gmail.com>
2019-08-23 17:21:57 +0530

    Log4J error corrected and implemented in AdminDbo.java

--- src/voteapplication/AdminDbo.java
+++ src/voteapplication/AdminDbo.java
@@ -17,6 +17,15 @@ public class AdminDbo {
     private Connection conn;
     private Logger applog;
 
+    public AdminDbo()
+    {
+        applog=Logger.getLogger(AdminDbo.class);
+        String str;
+        // Get Path of the current project folder
+        str=System.getProperty("user.dir");        
+        PropertyConfigurator.configure(str+"\\src\\voteapplication\\log4j.properties");                        
+    }
+    
     /* connectToDB() */
     public boolean connectToDB() {
         /* Create DB Connection */
@@ -59,13 +68,16 @@ public class AdminDbo {
             SQLStatement = conn.createStatement();
             ResultSet rs = SQLStatement.executeQuery("select * from Authentication where username='" + username + "' and password='" + password + "'");
 
-            if (rs.next()) {
+            if (rs.next()) 
+            {
+                applog.info("Authentication Successful!");
                 return true;
             }
         } catch (Exception e) {
             applog.error(e);
         }
         closeDbConnection();
+        applog.info("Authentication Failure!");
         return false;
 
     }
